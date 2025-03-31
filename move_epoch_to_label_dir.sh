ad1286737143db2dc716f27b6459ddf66f7adac0
Wachiii-Raya <wachiraya.chu@student.mahidol.edu>
2025-03-31 22:28:37 +0700

    update: train on 10s, 30s and 60s

---
+++ move_epoch_to_label_dir.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SOURCE_DIR="/Users/wachiii/Workschii/brain-mdd/data/dataset/predct/eyeopen_16Channels/epoch60sFiles"
+
+HC_DIR="$SOURCE_DIR/hc"
+MDD_DIR="$SOURCE_DIR/mdd"
+
+mkdir -p "$HC_DIR"
+mkdir -p "$MDD_DIR"
+
+for file in "$SOURCE_DIR"/*_HC*; do
+    if [ -f "$file" ]; then
+        echo "Moving $file to $HC_DIR"
+        mv "$file" "$HC_DIR"
+    fi
+done
+
+for file in "$SOURCE_DIR"/*_MDD*; do
+    if [ -f "$file" ]; then
+        echo "Moving $file to $MDD_DIR"
+        mv "$file" "$MDD_DIR"
+    fi
+done
+
+echo "File organization complete."
